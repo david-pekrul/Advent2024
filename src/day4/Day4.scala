29eafd076b1a58f376d902314e6f5ba045670d89
David Pekrul <dbighead77@gmail.com>
2024-12-04 21:33:52 -0600

    Day 4 part 2

--- src/day4/Day4.scala
+++ src/day4/Day4.scala
@@ -5,15 +5,27 @@ import helpers.Helpers
 object Day4 {
   def main(args: Array[String]): Unit = {
 //    val input = Helpers.readFile("src/day4/test3.txt").toSeq.map(_.toCharArray.toSeq)
+//    val input = Helpers.readFile("src/day4/test4.txt").toSeq.map(_.toCharArray.toSeq)
     val input = Helpers.readFile("src/day4/day4.txt").toSeq.map(_.toCharArray.toSeq)
 
     val part1 = solvePart1(input)
     println(s"Part 1: $part1")
 
+
+    //2069 too high
+    val part2 = solvePart2(input)
+    println(s"Part 2: $part2")
+
+
   }
 
   def solvePart1(input: Seq[Seq[Char]]): Long = {
     val gridSearcher = new GridSearcher(new Grid(input))
+    gridSearcher.countGrid()
+  }
+
+  def solvePart2(input: Seq[Seq[Char]]): Long = {
+    val gridSearcher = new GridSearcher2(new Grid(input))
     gridSearcher.countGrid()
   }
 }
@@ -47,7 +59,7 @@ class GridSearcher(grid: Grid) {
     })
   }
 
-  private def horizontal(x: Int, y: Int, stepDirection: Int = 1): Long = {
+  protected def horizontal(x: Int, y: Int, stepDirection: Int = 1): Long = {
     if FIND_WORD.forall(findCharAndIndex => {
       grid.getCharAtCoordinate(x + (stepDirection * findCharAndIndex._2), y).contains(findCharAndIndex._1)
     }) then
@@ -56,7 +68,7 @@ class GridSearcher(grid: Grid) {
       0
   }
 
-  private def vertical(x: Int, y: Int, stepDirection: Int = 1): Long = {
+  protected def vertical(x: Int, y: Int, stepDirection: Int = 1): Long = {
     if FIND_WORD.forall(findCharAndIndex => {
       grid.getCharAtCoordinate(x, y + (stepDirection * findCharAndIndex._2)).contains(findCharAndIndex._1)
     }) then
@@ -65,7 +77,7 @@ class GridSearcher(grid: Grid) {
       0
   }
 
-  private def diagonal1(x: Int, y: Int, stepDirection: Int = 1): Long = {
+  protected def diagonal1(x: Int, y: Int, stepDirection: Int = 1): Long = {
     if FIND_WORD.forall(findCharAndIndex => {
       grid.getCharAtCoordinate(x + (stepDirection * findCharAndIndex._2), y + (stepDirection * findCharAndIndex._2)).contains(findCharAndIndex._1)
     }) then
@@ -74,7 +86,7 @@ class GridSearcher(grid: Grid) {
       0
   }
 
-  private def diagonal2(x: Int, y: Int, stepDirection: Int = 1): Long = {
+  protected def diagonal2(x: Int, y: Int, stepDirection: Int = 1): Long = {
     if FIND_WORD.forall(findCharAndIndex => {
       grid.getCharAtCoordinate(x + (stepDirection * findCharAndIndex._2), y - (stepDirection * findCharAndIndex._2)).contains(findCharAndIndex._1)
     }) then
@@ -82,6 +94,24 @@ class GridSearcher(grid: Grid) {
     else
       0
   }
+}
 
+class GridSearcher2(grid: Grid) extends GridSearcher(grid) {
+  override val FIND_WORD: Seq[(Char, Int)] = "MAS".toCharArray.toSeq.zipWithIndex
+    .map{ case (c,i) => (c,i-1)} //this shift allows it to be centered on the "A"
 
+  override def countGrid(): Long = {
+    (0 until grid.height).foldLeft(0L)((outerAcc, y) => {
+      outerAcc + (0 until grid.width).foldLeft(0L)((innerAcc, x) => {
+        if(grid.getCharAtCoordinate(x,y).contains('A')) {
+          val diags = Math.max(diagonal1(x, y),diagonal1(x, y, -1))*Math.max(diagonal2(x, y),diagonal2(x, y, -1))
+
+          innerAcc
+            + diags
+        } else {
+          innerAcc
+        }
+      })
+    })
+  }
 }
