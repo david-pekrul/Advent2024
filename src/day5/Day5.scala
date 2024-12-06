ee5a49e26bd8d87e3e2fbec9e64d7a5b8989112b
David Pekrul <dbighead77@gmail.com>
2024-12-05 22:42:18 -0600

    Day 5 part 2

--- src/day5/Day5.scala
+++ src/day5/Day5.scala
@@ -7,13 +7,19 @@ object Day5 {
 //    val input = Helpers.readFile("src/day5/test.txt").map(_.trim)
     val input = Helpers.readFile("src/day5/day5.txt").map(_.trim)
 
-    val rules = input.takeWhile(_.nonEmpty).map(new PageRule(_)).toSet
+    val rules = input.takeWhile(_.nonEmpty).map(new PageRule(_))
     val orders = input.drop(rules.size+1).map(new PrintOrder(_))
 
     val inOrderAlready = orders.filter(order => order.isInOrder(rules))
     val part1 = inOrderAlready.map(_.middleValue()).sum
 
     println(s"Part 1: $part1 ")
+
+    val outOfOrder = orders.filter(!_.isInOrder(rules))
+    val sortedOrders = outOfOrder.map(a => (a,a.sort(rules)))
+
+    val part2 = sortedOrders.map(x => PrintOrder.middleValue(x._2)).sum
+    println(s"Part 2: $part2")
 
   }
 
@@ -22,19 +28,27 @@ object Day5 {
 object PageRule {
   val regex = """(\d+)\|(\d+)""".r
 }
+
 class PageRule(raw: String) {
   val PageRule.regex(leftString,rightString) = raw
   val left = leftString.toInt
   val right = rightString.toInt
 }
 
+object PrintOrder {
+  def middleValue(pagesAndIndexes: Map[Int, Int]): Int = {
+    val middle = pagesAndIndexes.find(_._2 == (pagesAndIndexes.size / 2)).get._1
+    middle
+  }
+}
+
 class PrintOrder(raw: String) {
   val pagesAndIndexes: Map[Int, Int] = raw.split(",").map(_.toInt).toSeq.zipWithIndex.toMap
 
-  def isInOrder(rules: Set[PageRule]): Boolean = {
+  def isInOrder(rules: Seq[PageRule], currentMap: Map[Int,Int] = pagesAndIndexes): Boolean = {
     rules.forall(rule => {
-      val leftOrder = pagesAndIndexes.get(rule.left)
-      val rightOrder = pagesAndIndexes.get(rule.right)
+      val leftOrder = currentMap.get(rule.left)
+      val rightOrder = currentMap.get(rule.right)
       (leftOrder, rightOrder) match {
         case (None,_) => true
         case (_,None) => true
@@ -47,7 +61,37 @@ class PrintOrder(raw: String) {
   }
 
   def middleValue(): Int = {
-    val middle = pagesAndIndexes.find(_._2 == (pagesAndIndexes.size/2)).get._1
-    middle
+    PrintOrder.middleValue(pagesAndIndexes)
   }
+
+  def sort(allRules: Seq[PageRule]): Map[Int,Int] = {
+    val filteredRules = findRulesThatApply(allRules)
+
+
+    def sortRecursive(remainingRules: Seq[PageRule], currentOrdering: Map[Int,Int]): Map[Int,Int] = {
+        if(remainingRules.isEmpty) {
+          return currentOrdering
+        }
+        val nextRule = remainingRules.head
+        if(isInOrder(Seq(nextRule),currentOrdering)) {
+          sortRecursive(remainingRules.tail, currentOrdering)
+        } else {
+          //swap and then add all the rules back
+          val oldLeft = currentOrdering(nextRule.left)
+          val oldRight = currentOrdering(nextRule.right)
+          val updatedMap = currentOrdering.updated(nextRule.left,oldRight).updated(nextRule.right,oldLeft)
+          sortRecursive(allRules,updatedMap) //start the search over again
+        }
+    }
+
+    //start the search
+    sortRecursive(allRules,pagesAndIndexes)
+  }
+
+  private def findRulesThatApply(rules: Seq[PageRule]): Seq[PageRule] = {
+    rules.filter(rule => {
+      pagesAndIndexes.contains(rule.left) && pagesAndIndexes.contains(rule.right)
+    })
+  }
+
 }
