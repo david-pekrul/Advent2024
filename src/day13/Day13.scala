4748315adf6c5232ccd3b33d11e6c67e46288b10
David Pekrul <dbighead77@gmail.com>
2024-12-14 19:00:42 -0600

    Day 13 part 2

--- src/day13/Day13.scala
+++ src/day13/Day13.scala
@@ -10,9 +10,14 @@ object Day13 {
     val machines = ClawMachine.parse(input)
 
     val solutions = machines.map(_.getCost())
-
     val part1 = solutions.map(_.getOrElse(0L)).sum
     println(s"Part 1: $part1")
+
+    val solutions2 = machines.map(_.getCost(None,Some(10000000000000L)))
+    val part2 = solutions2.map(_.getOrElse(0L)).sum
+    println(s"Part 2: $part2")
+    //76246593362537 too high
+    //73267584326867
   }
 }
 
@@ -36,13 +41,13 @@ object ClawMachine {
 
 class ClawMachine(val aX: Long, val aY: Long, val bX: Long, val bY: Long, val X: Long, val Y: Long) {
 
-  def getCost(): Option[Long] = {
+  def getCost(buttonLimit: Option[Long] = Some(100L), addTarget: Option[Long] = None): Option[Long] = {
 
     val lcmA = Helpers.lcm(Seq(aX,aY))
     val m1 = lcmA / aX;
     val m2 = lcmA / aY;
 
-    val bTop = (X*m1 - Y*m2)
+    val bTop = ((X+addTarget.getOrElse(0L))*m1 - (Y+addTarget.getOrElse(0L))*m2)
     val bBot = (bX*m1 - bY*m2)
 
     val mod = bTop%bBot
@@ -52,16 +57,26 @@ class ClawMachine(val aX: Long, val aY: Long, val bX: Long, val bY: Long, val X:
     }
 
     val B = bTop/bBot
-    
-    val A = (X - B*bX)/(aX)
 
-    if A > 100 || B > 100 then {
-      return None
+    val A = ((X+addTarget.getOrElse(0L)) - B*bX)/(aX)
+
+    if buttonLimit.isDefined then {
+      if A > buttonLimit.get || B > buttonLimit.get then {
+        return None
+      }
     }
-    
+
     if (A < 0 || B < 0) then {
       return None
     }
-    return Some(3*A + B)
+    
+    if((A*aX + B*bX) != (X+addTarget.getOrElse(0L))) then {
+      return None
+    } 
+    if((A*aY + B*bY) != (Y+addTarget.getOrElse(0L))) then {
+      return None
+    }
+    val cost = 3*A + B
+    return Some(cost)
   }
 }
